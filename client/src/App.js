07615ff4b430aaf4eda79787c12acdc53d40869f
ivan-yung <iyungvan@gmail.com>
2024-09-21 16:49:04 -0700

    link change on production client side

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('https://joeboxback.onrender.com/');
+const socket = socketIO.connect('https://joeboxback.onrender.com');
 function App() {
     return (
         <BrowserRouter>
