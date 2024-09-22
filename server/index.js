017fced267ef02ec20fac3274d774992cc2be98d
ivan-yung <iyungvan@gmail.com>
2024-09-21 17:02:25 -0700

    update CORS

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.static(staticPath));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://joebox.onrender.com"],
+    origin: ["https://joebox.onrender.com"],
     methods: ["GET", "POST"]
   }
 });
