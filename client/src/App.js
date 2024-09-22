08a7c7fbb097cbdade8e5f6e23f54b7bab194f3c
ivan-yung <iyungvan@gmail.com>
2024-09-21 17:13:55 -0700

    update socket port2

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-// const socket = socketIO.connect('https://joeboxback.onrender.com');
-var socket = io('https://joeboxback.onrender.com', { transports : ['websocket'] });
+const socket = socketIO.connect('https://joeboxback.onrender.com');
+// var socket = io('https://joeboxback.onrender.com', { transports : ['websocket'] });
 function App() {
     return (
         <BrowserRouter>
@@ -19,3 +19,10 @@ function App() {
 }
 
 export default App;
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://joebox.onrender.com'); // Allow requests from joebox.onrender.com
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify allowed HTTP methods
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
+//     next();
+//   });
